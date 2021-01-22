532c0e4b20c7b2b22c3b591152009c59c603b8db
elgondu@gmail.com <elgondu@gmail.com>
2021-01-21 23:30:56 -0300

    commend observers

--- reactive-rxjs/reactive-rxjs/src/observables/03-subject.ts
+++ reactive-rxjs/reactive-rxjs/src/observables/03-subject.ts
@@ -8,8 +8,8 @@ const observer: Observer<any> = {
 
 const intervalo$ = new Observable<number>( subs => {
 
-    const intervalID = setInterval( 
-      () => subs.next( Math.random() ), 1000 
+    const intervalID = setInterval(
+      () => subs.next( Math.random() ), 1000
     );
 
     return () => {
@@ -29,11 +29,11 @@ const subject$ = new Subject();
 const subscription = intervalo$.subscribe( subject$ );
 
 
-// const subs1 = intervalo$.subscribe( rnd => console.log('subs1', rnd) );
-// const subs2 = intervalo$.subscribe( rnd => console.log('subs2', rnd) );
+ const subs1 = intervalo$.subscribe( rnd => console.log('subs1', rnd) );
+ const subs2 = intervalo$.subscribe( rnd => console.log('subs2', rnd) );
 
-const subs1 = subject$.subscribe( observer );
-const subs2 = subject$.subscribe( observer );
+//const subs1 = subject$.subscribe( observer );
+//const subs2 = subject$.subscribe( observer );
 
 
 setTimeout( () => {

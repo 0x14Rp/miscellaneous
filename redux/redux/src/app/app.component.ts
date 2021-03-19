ddaa39f2ecc60b3b4634a03a5d53d2b07e2098c8
elgondu@gmail.com <elgondu@gmail.com>
2021-03-18 22:26:27 -0300

    counter with inputs and outputs

--- redux/redux/src/app/app.component.ts
+++ redux/redux/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redux';
+
+  contador:number
+
+  constructor() {
+    this.contador = 10;
+
+  }
+  incrementar(){
+    this.contador ++;
+  }
+
+  decrementar(){
+    this.contador --;
+  }
+
 }
--- redux/redux/src/app/app.module.ts
+++ redux/redux/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HijoComponent } from './contador/hijo/hijo.component';
+import { NietoComponent } from './contador/nieto/nieto.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HijoComponent,
+    NietoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]

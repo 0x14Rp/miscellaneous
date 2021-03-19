d4fe0043431ede9035f76f6147b902bb4dd4964f
elgondu@gmail.com <elgondu@gmail.com>
2021-03-18 23:10:43 -0300

    actions

--- redux/redux/src/app/app.component.ts
+++ redux/redux/src/app/app.component.ts
@@ -1,25 +1,40 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Store} from "@ngrx/store";
+import * as actions from "./contador/contador.actions";
+
+
+interface AppState {
+  contador: number;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'redux';
 
-  contador:number
+  contador: number
 
-  constructor() {
-    this.contador = 10;
+  constructor(private store: Store<AppState>) {
+
+    this.store.subscribe(state => {
+      console.log(state);
+      this.contador = state.contador;
+    })
 
   }
-  incrementar(){
-    this.contador ++;
+
+  incrementar() {
+    this.store.dispatch(actions.incrementar())
   }
 
-  decrementar(){
-    this.contador --;
+  decrementar() {
+    this.store.dispatch(actions.decrementar())
   }
 
 }
--- redux/redux/src/app/app.module.ts
+++ redux/redux/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HijoComponent } from './contador/hijo/hijo.component';
-import { NietoComponent } from './contador/nieto/nieto.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {AppComponent} from './app.component';
+import {HijoComponent} from './contador/hijo/hijo.component';
+import {NietoComponent} from './contador/nieto/nieto.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {StoreModule} from '@ngrx/store';
+import {contadorReducer} from "./contador/contador.reducer";
 
 @NgModule({
   declarations: [
@@ -14,9 +16,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    StoreModule.forRoot({contador: contadorReducer}),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
